e46027635773f4fee2028ec9f58d6bd7140327a0
Luke VanderHart <luke@thinkrelevance.com>
2011-09-30 11:14:27 -0400

    Foreign library example

--- script/compile.clj
+++ script/compile.clj
@@ -1,5 +1,11 @@
+;; cx, cy, r become p, r, g. fill and stroke are fine.
+;; e.g, using 'attr' works fine, the constructor functions screw it up.
+
 (require '[cljs.closure :as closure])
 
+
 (closure/build "src/cljs" {:optimizations :advanced
+                           :foreign-libs [{:file "https://raw.github.com/DmitryBaranovskiy/raphael/master/raphael-min.js"
+                                           :provides ["raphael"]}]
                            :output-dir "working"
                            :output-to "public/js/main.js"})
--- src/cljs/hello.cljs
+++ src/cljs/hello.cljs
@@ -1,11 +1,14 @@
 (ns template.hello
-  (:require [goog.dom :as dom]))
+  (:require [goog.dom :as dom]
+            [raphael :as raphael]))
 
 (defn ^:export init []
   (js/alert "Hello!")
-  (def paper (Raphael. 10 50 320 200))
+
+  (def paper (js/Raphael. 10 50 320 200))
 
   (def circle (. paper (circle 50 40 10)))
 
+  (. circle (attr "r" 100))
   (. circle (attr "fill" "#f00"))
   (. circle (attr "stroke" "#00f")))
